400ab5d8743f2ac48718ac896612b64f1da41c71
Christopher Mayfield <chrismay4115@gmail.com>
2023-05-02 01:15:59 -0400

    fixed distanceMatrixService requests

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,7 +5,6 @@ import {
   CircleF,
   StandaloneSearchBox,
   MarkerF,
-  DistanceMatrixService,
   DirectionsRenderer,
 } from "@react-google-maps/api";
 import { env } from "~/env.mjs";
@@ -25,6 +24,7 @@ import {
 } from "./ui/sheet";
 import { CheckIcon } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const LIBRARIES: (
   | "drawing"
@@ -86,8 +86,8 @@ function MyComponent() {
   }, [selectedPlace, fetchDirections]);
 
   return geoPosition ? (
-    <div className="mx-auto flex h-full w-full justify-center  px-4">
-      <div className="h-full w-full md:h-[700px] md:w-[1000px]">
+    <div className="mx-auto flex h-full w-full justify-center px-4">
+      <div className="h-full w-full overflow-hidden rounded-lg md:h-[700px] md:w-[1000px]">
         <LoadScriptNext
           googleMapsApiKey={env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
           libraries={LIBRARIES}
@@ -207,7 +207,7 @@ function MyComponent() {
           </GoogleMap>
         </LoadScriptNext>
       </div>
-      <div className="relative h-full max-h-[700px] w-96 flex-col overflow-auto border bg-neutral-200">
+      <div className="relative ml-6 h-full max-h-[700px] w-96 flex-col overflow-auto rounded-lg border bg-neutral-200">
         <h2 className="sticky top-0 z-50 w-full border-b-2 border-blue-500 bg-inherit p-2 px-8 text-xl font-semibold">
           Search Results
         </h2>
@@ -248,94 +248,36 @@ const secondsToDuration = (time: number): Duration => {
   };
 };
 
-const PlaceCard = ({
-  place,
-  index: i,
-  map,
-  geoPosition,
-  setSelectedPlace,
-  selectedPlace,
-}: {
-  place: google.maps.places.PlaceResult;
-  index: number;
-  map: google.maps.Map | null;
-  geoPosition: {
-    latitude: number;
-    longitude: number;
-  };
-  setSelectedPlace: React.Dispatch<
-    React.SetStateAction<google.maps.places.PlaceResult | null>
-  >;
-  selectedPlace: google.maps.places.PlaceResult | null;
-}) => {
-  const [distance, setDistance] = useState<string | null>(null);
-  const [duration, setDuration] = useState<
-    google.maps.Duration | undefined | null
-  >(null);
+const PlaceCard = memo(
+  ({
+    place,
+    index: i,
+    map,
+    geoPosition,
+    setSelectedPlace,
+    selectedPlace,
+  }: {
+    place: google.maps.places.PlaceResult;
+    index: number;
+    map: google.maps.Map | null;
+    geoPosition: {
+      latitude: number;
+      longitude: number;
+    };
+    setSelectedPlace: React.Dispatch<
+      React.SetStateAction<google.maps.places.PlaceResult | null>
+    >;
+    selectedPlace: google.maps.places.PlaceResult | null;
+  }) => {
+    const [distance, setDistance] = useState<string | null>(null);
+    const [duration, setDuration] = useState<
+      google.maps.Duration | undefined | null
+    >(null);
 
-  return (
-    <li
-      key={`${i}${place.name ?? ""}`}
-      className={`relative p-2 px-8 hover:bg-gray-300 ${
-        selectedPlace?.place_id === place.place_id ? "bg-blue-300" : ""
-      }`}
-      onClick={() => {
-        if (map && place.geometry?.location) {
-          map.setCenter(place.geometry.location);
-        }
-        setSelectedPlace(place);
-      }}
-    >
-      {selectedPlace?.place_id === place.place_id && (
-        <div className="absolute top-0 -left-3.5">
-          <CheckIcon className="h-5 w-5 text-red-500" />
-        </div>
-      )}
-      <div className="flex items-center">
-        <span>{i + 1}</span> -{" "}
-        <span className="text-lg font-semibold">{place.name}</span>
-      </div>
-      <div>{place.formatted_address}</div>
-      <div>Distance: {distance?.slice(0, 4)} Miles</div>
-      {duration && (
-        <div>Duration: {formatDuration(secondsToDuration(duration.value))}</div>
-      )}
-      <Sheet>
-        <SheetTrigger className="ml-auto mt-2 flex items-center justify-center rounded bg-blue-500 px-4 py-2 hover:bg-blue-700 hover:text-white">
-          More Info
-        </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle className="text-2xl font-semibold">
-              {place.name}
-            </SheetTitle>
-            <SheetDescription>
-              <div>Address: {place.formatted_address}</div>
-              <div>
-                Phone:{" "}
-                {place.formatted_phone_number ??
-                  "Whoops! Looks like this place doesn't have a number!"}
-              </div>
-              {place.url && (
-                <Link href={place.url} className="hover:underline">
-                  <LinkIcon className="h-5 w-5" /> {place.url}
-                </Link>
-              )}
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-      <DistanceMatrixService
-        callback={(res) => {
-          setDistance(
-            String(
-              metersToMiles(res?.rows[0]?.elements[0]?.distance.value ?? 0) ??
-                ""
-            )
-          );
-          setDuration(res?.rows[0]?.elements[0]?.duration);
-        }}
-        options={{
+    const getDistanceMatrix = useCallback(() => {
+      const service = new google.maps.DistanceMatrixService();
+      void service.getDistanceMatrix(
+        {
           origins: [
             new google.maps.LatLng({
               lat: geoPosition.latitude,
@@ -348,10 +290,118 @@ const PlaceCard = ({
             },
           ],
           travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (res) => {
+          setDistance(
+            String(
+              metersToMiles(res?.rows[0]?.elements[0]?.distance.value ?? 0) ??
+                ""
+            )
+          );
+          setDuration(res?.rows[0]?.elements[0]?.duration);
+        }
+      );
+    }, [place.place_id, geoPosition]);
+
+    useEffect(() => {
+      getDistanceMatrix();
+    }, [getDistanceMatrix]);
+
+    return (
+      <li
+        key={`${i}${place.name ?? ""}`}
+        className={`relative p-2 px-8 hover:bg-gray-300 ${
+          selectedPlace?.place_id === place.place_id ? "bg-blue-300" : ""
+        }`}
+        onClick={() => {
+          if (map && place.geometry?.location) {
+            map.setCenter(place.geometry.location);
+          }
+          setSelectedPlace(place);
         }}
-      />
-    </li>
-  );
-};
+      >
+        {selectedPlace?.place_id === place.place_id && (
+          <div className="absolute top-0 -left-3.5">
+            <CheckIcon className="h-5 w-5 text-red-500" />
+          </div>
+        )}
+        <div className="flex items-center">
+          <span>{i + 1}</span> -{" "}
+          <span className="text-lg font-semibold">{place.name}</span>
+        </div>
+        <div>{place.formatted_address}</div>
+        <div>Distance: {distance?.slice(0, 4)} Miles</div>
+        {duration && (
+          <div>
+            Duration: {formatDuration(secondsToDuration(duration.value))}
+          </div>
+        )}
+        <Sheet>
+          <SheetTrigger className="ml-auto mt-2 flex items-center justify-center rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-700">
+            More Info
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="text-3xl font-semibold">
+                {place.name}
+              </SheetTitle>
+              <SheetDescription className="flex flex-col space-y-4">
+                {place.photos &&
+                  place.photos.slice(0, 1).map((photo) => {
+                    const src = photo.getUrl({ maxHeight: 500, maxWidth: 500 });
+
+                    return src ? (
+                      <Image
+                        key={place.place_id}
+                        src={src}
+                        className="my-4 mt-auto h-full w-full max-w-sm"
+                        alt={`${place.name ?? ""} picture`}
+                        width={100}
+                        height={100}
+                      />
+                    ) : null;
+                  })}
+                {place.rating && (
+                  <div>
+                    User Ratings:{" "}
+                    <span className="font-bold">{place.rating}</span>{" "}
+                    {place.user_ratings_total && (
+                      <>
+                        out of{" "}
+                        <span className="font-bold">
+                          {place.user_ratings_total}
+                        </span>{" "}
+                        reviews.
+                      </>
+                    )}
+                  </div>
+                )}
+                <div>
+                  Address:{" "}
+                  <span className="font-bold">{place.formatted_address}</span>
+                </div>
+                <div>
+                  Phone:{" "}
+                  {place.formatted_phone_number ??
+                    "Whoops! Looks like this place doesn't have a number!"}
+                </div>
+                {place.url && (
+                  <Link
+                    href={place.url}
+                    className="flex items-center space-x-2 hover:underline"
+                  >
+                    <LinkIcon className="h-4 w-4" /> <span>{place.url}</span>
+                  </Link>
+                )}
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </li>
+    );
+  }
+);
+
+PlaceCard.displayName = "PlaceCard";
 
 export default memo(MyComponent);
